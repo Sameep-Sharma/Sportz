f51e348fe644f4da6692edc4f3c6cdfc5ce366dc
Sameep-Sharma <sharmasameep94@gmail.com>
2026-08-22 11:22:58 +0530

    implement suggested fixes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import http from "http";
 import { attachWebSocketServer } from "./ws/server.js";
 
 const PORT = Number(process.env.PORT || 8000);
-const HOST = Number(process.env.HOST || "0.0.0.0");
+const HOST = process.env.HOST || "0.0.0.0";
 
 const app = express();
 
@@ -22,7 +22,7 @@ const { broadCastMatchCreated } = attachWebSocketServer(server);
 app.locals.broadCastMatchCreated = broadCastMatchCreated;
 
 server.listen(PORT,HOST, () => {
-  const baseUrl = HOST === '0.0.0.0' ? `http://localhost${PORT}` : `http://${HOST}:${PORT}`;
+  const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
 
   console.log(`Server is running on ${baseUrl}`);
   console.log(`Web Socket Server is running on ${baseUrl.replace('http','ws')}/ws`);
--- src/ws/server.js
+++ src/ws/server.js
@@ -10,7 +10,7 @@ function sendJson(socket, payload) {
 
 function broadCast(wss, payload) {
   for (const client of wss.clients) {
-    if (client.readyState !== WebSocket.OPEN) return;
+    if (client.readyState !== WebSocket.OPEN) continue;
 
     client.send(JSON.stringify(payload));
   }
@@ -24,14 +24,29 @@ export function attachWebSocketServer(server) {
   });
 
   wss.on("connection", (socket) => {
+    socket.isAlive = true;
+    socket.on("pong", () => {
+      socket.isAlive = true;
+    });
     sendJson(socket, { type: "welcome" });
 
-    socket.on('error', console.error);
+    socket.on("error", console.error);
   });
 
-  function broadCastMatchCreated(match){
-    broadCast(wss, { type: 'match_created' , data: match});
+  const interval = setInterval(() => {
+    (wss.clients,
+      forEach((ws) => {
+        if (ws.isAlive === false) return ws.terminate();
+        ws.isAlive = false;
+        ws.ping();
+      }));
+  }, 3000);
+
+  wss.on("close", () => clearInterval(interval));
+
+  function broadCastMatchCreated(match) {
+    broadCast(wss, { type: "match_created", data: match });
   }
 
-  return { broadCastMatchCreated }
+  return { broadCastMatchCreated };
 }
